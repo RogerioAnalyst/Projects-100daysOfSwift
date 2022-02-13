0d95ec0b25786109ff8276964c9655b4d41a51e3
Rogerio Cardoso Filho <rogerinho.cardosot3@gmail.com>
2022-02-12 22:06:16 -0300

    Add files via upload
    Project 2

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Rogerio Cardoso Filho on 12/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var contries = [String]()
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        contries += ["estonia","france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
+        
+    }
+    
+    func askQuestion(){
+        
+        button1.setImage(UIImage(named: contries[0]), for: .normal)
+        button2.setImage(UIImage(named: contries[1]), for: .normal)
+        button3.setImage(UIImage(named: contries[2]), for: .normal)
+        
+    }
+}
