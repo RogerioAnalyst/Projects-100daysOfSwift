09579aa5d2975b4e6ad1d68c0a6bc68082e388c9
Rogerio Cardoso <dev.rogeriof@gmail.com>
2022-02-14 21:13:03 -0300

    challenge solved

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -16,11 +16,17 @@ class ViewController: UIViewController {
     var contries = [String]()
     var score = 0
     var correctAnshwer = 0
+    var numberOfQuestions = 0
+    var highScore = 0
+    
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         contries += ["estonia","france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "spain", "uk", "us"]
+        
+        askQuestion()
         
         button1.layer.borderWidth = 1
         button2.layer.borderWidth = 1
@@ -30,9 +36,11 @@ class ViewController: UIViewController {
         button2.layer.borderColor = UIColor.lightGray.cgColor
         button3.layer.borderColor = UIColor.lightGray.cgColor
         
-        askQuestion()
-        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(displayScore))
+                
     }
+    
+    //MARK: - askQuestion
     
     func askQuestion(action: UIAlertAction! = nil){
         
@@ -47,24 +55,93 @@ class ViewController: UIViewController {
         
     }
     
+    //MARK: - buttonTapped
+    
     @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.15, initialSpringVelocity: 5, options: [.curveEaseIn], animations: {
+            sender.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+        }) { finished in
+            sender.transform = .identity
+        }
         
         var title: String
         
         if sender.tag == correctAnshwer {
             title = "Correct"
             score += 1
+            numberOfQuestions += 1
+            
         } else {
-            title = "Wrong"
+            title = "Wrong, that's the flag of: \(contries[sender.tag].uppercased())"
             score -= 1
+            numberOfQuestions += 1
+            
         }
         
-        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        if numberOfQuestions == 10{
+            
+            if highScore < score {
+                highScore = score
+                save()
+            }
+            
+            let final = UIAlertController(title: "You're final score is:", message: "\(score)", preferredStyle: .alert)
+            final.addAction(UIAlertAction(title: "Restart", style: .default, handler: askQuestion))
+            final.addAction(UIAlertAction(title: "View HighScore", style: .default) { [weak self] _ in
+                let ac = UIAlertController(title: "Your HighScore is: \(self!.highScore)", message: nil, preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "Restart", style: .cancel, handler: self?.askQuestion))
+                self?.present(ac, animated: true)
+            })
+            present(final, animated: true)
+            
+            score = 0
+            
+            numberOfQuestions = 0
+            
+        } else {
+            
+            let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+            
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            
+            present(ac, animated: true)
+            
+        }
         
-        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        let label = UILabel()
+        label.text = "\(score)"
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = UIColor.black
         
-        present(ac, animated: true)
+        let barButton1 = UIBarButtonItem(customView: label)
+        
+        self.navigationItem.setRightBarButtonItems([barButton1], animated: false)
         
     }
+    
+    //MARK: - displayScore
+    
+    @objc func displayScore() {
+           let vc = UIAlertController(title: "Youre score is:", message: "\(score)", preferredStyle: .alert)
+           vc.addAction(UIAlertAction(title: "Continue to Play Guess The Flag", style: .default))
+           vc.addAction(UIAlertAction(title: "Share youre score", style: .default, handler: shareScore))
+           present(vc, animated: true)
+       }
+       
+    //MARK: - shareScore
+    
+       func shareScore(action: UIAlertAction! = nil) {
+           let message = "I have \(score) points."
+           let vc = UIActivityViewController(activityItems: [message], applicationActivities: [])
+           vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+           present(vc, animated: true)
+       }
+    
+    //MARK: - save
+       
+       func save() {
+           defaults.set(highScore, forKey: "highScore")
+       }
     
 }
